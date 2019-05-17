588a7b82802798d1fc4970ab26e6eb978d1f4669
liuheng <1713856662a@gmail.com>
2019-05-17 15:23:46 +0800

    更新gitignore

---
+++ converter/sub_snipthumb.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: cicada
+@contact: 1713856662a@gmail.com
+@file: sub_snipthumb.py
+@time: 2019/5/16 上午10:15 
+"""
+
---
+++ core/CFMesh.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: cicada
+@contact: 1713856662a@gmail.com
+@file: CFMesh.py
+@time: 2019/5/17 上午9:48 
+"""
+
