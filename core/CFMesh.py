1c40c89af57618e2df944bdf4107cd34c28d5b9c
liuheng <1713856662a@gmail.com>
2019-05-24 18:18:21 +0800

    更新gitignore

--- core/CFMesh.py
+++ core/CFMesh.py
@@ -4,12 +4,6 @@
 @author: cicada
 @contact: 1713856662a@gmail.com
 @file: CFMesh.py
-@time: 2019/5/17 上午9:48 
+@time: 2019/5/21 上午10:54 
 """
-from core.MeshOpt import MeshOpt
 
-
-class CFMesh(MeshOpt):
-
-    def __new__(cls, mesh_dir, mesh_app_type=4, ):
-        pass
---
+++ core/CFMesh2D.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: cicada
+@contact: 1713856662a@gmail.com
+@file: CFMesh.py
+@time: 2019/5/17 上午9:48 
+"""
+from typing import Dict
+
+from core.MeshOpt import MeshOpt
+
+
+class CFMesh2D(MeshOpt):
+
+    def __new__(cls, cad_file, mesh_dir, mesh_config, mesh_app=0, **kwargs):
+        pass
